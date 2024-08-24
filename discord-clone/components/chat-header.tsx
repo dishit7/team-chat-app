ca6290985189dd7a42d348c400890163fad92fdd
dishit7.dev <hanmadishit74@gmail.com>
2024-08-24 23:24:09 +0530

    completed Chat-Header Component

--- discord-clone/components/chat-header.tsx
+++ discord-clone/components/chat-header.tsx
@@ -1,6 +1,5 @@
-import { Hash, Menu } from "lucide-react"
+ import { Hash, Menu } from "lucide-react"
 import { MobileToggle } from "./mobile-toggle"
-
 interface ChatHeaderProps{
     serverId:string,
     name:string,
@@ -10,7 +9,7 @@ interface ChatHeaderProps{
 export function ChatHeader({serverId,name,type,imageUrl}:ChatHeaderProps){
     return (
         <div className="flex items-center px-3  font-semibold text-base border-b-2 border-neutral-200 dark:border-neutral-800 h-12">
-        <MobileToggle/>
+        <MobileToggle serverId={serverId}/>
         {type==="channel"&& (<Hash className="h-5 w-5 mr-2"/>  )}<p>{name}</p>
         </div>
     )
--- discord-clone/components/mobile-toggle.tsx
+++ discord-clone/components/mobile-toggle.tsx
@@ -1,13 +1,26 @@
-import { Menu } from "lucide-react";
+ import { Menu } from "lucide-react";
 import { Sheet,SheetTrigger,SheetContent,SheetTitle,SheetDescription } from "./ui/sheet"
-
-export function MobileToggle(){
+import NavigationSideBar from "./navigation/navigation-sidebar";
+import ServerSideBar from "./server-sidebar/server-sidebar";
+interface MobileToggleProps{
+    serverId:string
+}
+export function MobileToggle({serverId}:MobileToggleProps){
+    {console.log(`[MOBILE TOGGLE COMP] SERVERID IS ${serverId}`)}
     return (
         <div>
             <Sheet>
                 <SheetTrigger>
                     <Menu/>
                 </SheetTrigger>
+                <SheetContent side="left" className="p-0 flex gap-0">
+                    <div className="flex items-center h-full w-full">
+                    <NavigationSideBar/>
+                    <div className="bg-server-sidebar h-full">
+                    <ServerSideBar serverId={serverId}  />
+                    </div>
+                    </div>
+                </SheetContent>
             </Sheet>
         </div>
     )
