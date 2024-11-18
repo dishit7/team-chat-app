e8868a008165288b7ac67a6404b5696ab474e671
dishit7 <hanmadishit74@gmail.com>
2024-11-19 00:46:41 +0530

    Improve  app design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,16 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
-  theme: {screens: {
-    'sm': '640px',
-    'md': '768px',
-    'lg': '1024px',
-    'xl': '1280px',
-    '2xl': '1536px',
-  },
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     container: {
       center: true,
       padding: "2rem",
@@ -29,7 +30,7 @@ const config = {
         'server-sidebar': 'var(--server-sidebar-color)',
         'main-interface': 'var(--main-interface-color)',
         'text': 'var(--text-color)',
-        'channels-list-color':'var(--channels-list-color)',
+        'channels-list-color': 'var(--channels-list-color)',
 
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -65,6 +66,12 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      boxShadow: {
+        'neon': '0px 0px 15px 3px rgba(0, 255, 255, 0.7)',  // Neon glowing border
+      },
+      textShadow: {
+        'neon': '0px 0px 10px 2px rgba(0, 255, 255, 0.8)',  // Neon text glow
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -79,10 +86,43 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        'gradient-y': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'center top',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'center center',
+          }
+        },
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          }
+        },
+        'gradient-xy': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        'gradient-x': 'gradient-x 15s ease infinite',
+        'gradient-y': 'gradient-y 15s ease infinite',
+        'gradient-xy': 'gradient-xy 15s ease infinite',
       },
     },
   },
